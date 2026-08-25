3b49602f648d930c01b81c0e82be8ed4acfcf8c4
Matt Cox <mattcox27@gmail.com>
2026-08-25 06:52:05 +0100

    Sets VectorLike to conform to VectorProtocoll

--- Sources/Cartesian/Vector/Protocols/VectorLike.swift
+++ Sources/Cartesian/Vector/Protocols/VectorLike.swift
@@ -8,18 +8,36 @@
 
 /// A vector that can act as a Vector2.
 ///
-public protocol Vector2Like {
+public protocol Vector2Like: VectorProtocol {
 	
+}
+
+extension Vector2Like {
+	static var count: Int {
+		2
+	}
 }
 
 /// A vector that can act as a Vector3.
 ///
-public protocol Vector3Like {
+public protocol Vector3Like: VectorProtocol {
 
+}
+
+extension Vector3Like {
+	static var count: Int {
+		3
+	}
 }
 
 /// A vector that can act as a Vector4.
 ///
-public protocol Vector4Like {
+public protocol Vector4Like: VectorProtocol {
 
+}
+
+extension Vector4Like {
+	static var count: Int {
+		4
+	}
 }
